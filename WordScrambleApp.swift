75a06351fed7dddea9314630901133c8c3c3177e
Eddie Fu <99326813+eddiefu1@users.noreply.github.com>
2023-10-03 21:09:29 -0700

    Add files via upload

---
+++ WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Eddie Fu on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
